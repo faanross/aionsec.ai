98c84e7ccce9bda181837a018d40d0d1da84ff78
faanross <moi@faanross.com>
2026-01-07 21:15:25 -0500

    Clean up course page - remove leftover MailerLite reinit code

--- src/routes/course/+page.svelte
+++ src/routes/course/+page.svelte
@@ -6,38 +6,6 @@
 
 	onMount(() => {
 		mounted = true;
-
-		// Force MailerLite to reinitialize for SPA navigation
-		const reinitMailerLite = () => {
-			// Remove existing MailerLite script
-			const existingScripts = document.querySelectorAll('script[src*="mailerlite"]');
-			existingScripts.forEach(s => s.remove());
-
-			// Clear form container
-			const formContainer = document.querySelector('.ml-embedded');
-			if (formContainer) {
-				formContainer.innerHTML = '';
-			}
-
-			// Reload MailerLite
-			const script = document.createElement('script');
-			script.innerHTML = `
-				(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[])
-				.push(arguments);},l=d.createElement(e),l.async=1,l.src=u,
-				n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})
-				(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
-				ml('account', '1462037');
-			`;
-			document.head.appendChild(script);
-		};
-
-		// Check if form is empty (SPA navigation) and reinitialize
-		setTimeout(() => {
-			const formContainer = document.querySelector('.ml-embedded');
-			if (formContainer && formContainer.children.length === 0) {
-				reinitMailerLite();
-			}
-		}, 200);
 	});
 </script>
 
